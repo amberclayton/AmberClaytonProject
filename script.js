e570768b29bfb47cbd71771d83b3405645635d0f
Amber Clayton <amberclaytonaveda@gmail.com>
2019-03-09 19:29:20 -0500

    updated carousel

---
+++ script.js
@@ -0,0 +1,25 @@
+// SMOOTH SCROLL
+
+$(function() {
+  $("a[href*=#]:not([href=#])").on("click", function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          900
+        );
+        return false;
+      }
+    }
+  });
+});
+
+// End Smooth Scroll
