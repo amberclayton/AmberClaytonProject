24fb43b86bbd55c65f49e3d2c87f56eb67dac408
Amber Clayton <amberclaytonaveda@gmail.com>
2019-03-29 01:21:04 -0400

    final commit after a disaster

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   console.log("hi");
 
-  $(".img-amber").animate({ left: "500px" }, "fast");
-});
+  $("#img-amber").animate({ opacity: 1 }, 1500);
+
+
+  $("button").click(function(){
+    $("btn btn-primary").animate({
+      height: 'toggle'
+    });
+  }); 
